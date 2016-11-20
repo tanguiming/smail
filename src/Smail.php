322cb11694daaaf083f9d72d82bae317ff1593c6
vagrant <vagrant@homestead>
2016-11-20 14:02:54 +0000

    first commit

---
+++ src/Smail.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Test\Smail;
+
+
+
+class Smail{
+
+        /**
+     * Create a new Lumen application instance.
+     *
+     * @param  string|null  $basePath
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+    
+    public function index() {
+        echo "ddddaaadd test success";
+    }
+
+}
